e9bd243bb8dca2042d5ce7ca7646c6444ef07e06
Alex M <153501077+D31TO@users.noreply.github.com>
2025-05-15 17:01:13 +0100

    C++ proj transfer

---
+++ T3UBPart2/Source/T3UBPart2/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class T3UBPART2_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
